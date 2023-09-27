7263f88eded7734a2b84ced7b17453d7e15e77be
Adammossst <adamsaja331@gmail.com>
2023-09-27 09:24:47 +0700

    Kumpul jurnal01

---
+++ Jurnal01.java
@@ -0,0 +1,7 @@
+public class Jurnal01 {
+    public static void main(String[] args){
+        System.out.println("Nama: Adam Maulana Chaniago");
+        System.out.println("NIM: 607062300025");
+        System.out.println("Kelas: D3IF-47-04");
+    } 
+}
